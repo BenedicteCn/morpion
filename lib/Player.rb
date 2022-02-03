efd5d7b76f21b2fa5f4b0643bb02652ba5db049d
BenedicteCn <97516256+BenedicteCn@users.noreply.github.com>
2022-02-03 22:33:30 +0100

    Delete lib directory

--- lib/Player.rb
+++
@@ -1,9 +0,0 @@
-class Player # on crée la classe Player, ne contient que les infos sur les joueurs (points de vie, nom, attributs etc.)
-  attr_accessor :name, :symbol
-
-  def initialize(symbol = "#")
-    @name = "###"
-    @symbol = symbol
-  end
-
-end
